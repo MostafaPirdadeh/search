a79a6b061ac42663d2bc25cd6cacb824e3e07c35
Mostafa Pirdadeh <116708385+MostafaPirdadeh@users.noreply.github.com>
2023-08-19 23:56:42 +0330

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,19 @@ import { Users } from "./user";
 import Table from "./Table";
 function App() {
   const [query, setQuery] = useState("");
+
+  const search = (data) => {
+    return data.filter((item) => item.first_name.toLowerCase().includes(query));
+  };
   return (
     <div className="App">
-      <input type="text" placeholder="Search..." className="search" onChange={(e)=>setQuery(e.target.value)}/>
-      <Table data={Users}/>
+      <input
+        type="text"
+        placeholder="Search..."
+        className="search"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <Table data={search(Users)} />
     </div>
   );
 }
